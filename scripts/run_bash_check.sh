86e7c86d406ec4a0e6aa68b2360317f5cbe0c2a1
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-05-28 12:02:56 +0200

    Add bash style check

---
+++ scripts/run_bash_check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Run the bash style checker
+# 
+# Usage:
+#
+#   ./scripts/run_bash_check.sh
+
+if [[ "$PWD" =~ scripts$ ]]; then
+    echo "FATAL ERROR."
+    echo "Please run the script from the project root. "
+    echo "Present working director: $PWD"
+    echo " "
+    echo "Tip: like this"
+    echo " "
+    echo "  ./scripts/run_bash_check.sh"
+    echo " "
+    exit 42
+fi
+
+shellcheck $(find . | grep "sh$")
+
