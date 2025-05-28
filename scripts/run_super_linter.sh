e27eaf462fc6eb0b41fe9e3f34baf66f3bb35857
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-05-28 10:55:34 +0200

    Add comment

--- scripts/run_super_linter.sh
+++ scripts/run_super_linter.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+#
+# Run the Super Linter, with 
+# homepage at https://github.com/super-linter/super-linter
+# 
+# Usage:
+#
+#   ./scripts/run_super_linter.sh
+
+if [[ "$PWD" =~ scripts$ ]]; then
+    echo "FATAL ERROR."
+    echo "Please run the script from the project root. "
+    echo "Present working director: $PWD"
+    echo " "
+    echo "Tip: like this"
+    echo " "
+    echo "  ./scripts/run_super_linter.sh"
+    echo " "
+    exit 42
+fi
+
+
 docker run \
   -e LOG_LEVEL=DEBUG \
   -e RUN_LOCAL=true \
