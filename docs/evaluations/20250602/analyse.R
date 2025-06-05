2f4253a4dc2e479d09c6f07fda6a36268019c522
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-06-05 14:39:57 +0200

    First analysis

--- docs/evaluations/20250602/analyse.R
+++ docs/evaluations/20250602/analyse.R
@@ -1,7 +1,7 @@
 #!/bin/env Rscript
 
 read_data <- function() {
-  readr::read_csv("evaluation_20250428_day_3.csv", show_col_types = FALSE)
+  readr::read_csv("results.csv", show_col_types = FALSE)
 }
 
 t <- read_data()
@@ -11,7 +11,7 @@ t <- read_data()
 ratings <- t |> dplyr::select("Overall, how would you rate this training event?")
 names(ratings) <- "rating"
 ggplot2::ggplot(
-  ratings, 
+  ratings,
   ggplot2::aes(x = rating)
 ) + ggplot2::geom_histogram() +
   ggplot2::scale_x_continuous(
@@ -20,7 +20,7 @@ ggplot2::ggplot(
   ) +
   ggplot2::scale_y_continuous(
     name = "Frequency"
-  ) +  
+  ) +
   ggplot2::labs(
     title = "Course rating",
     caption = paste0(
@@ -47,7 +47,7 @@ recommend <- t |> dplyr::select(starts_with("Would you recommend this course to 
 names(recommend) <- "recommend"
 recommend$recommend <- as.factor(recommend$recommend)
 
-ggplot2::ggplot(recommend, ggplot2::aes(x = recommend)) + 
+ggplot2::ggplot(recommend, ggplot2::aes(x = recommend)) +
   ggplot2::geom_bar() +
   ggplot2::scale_y_continuous(
     name = "Number of learners"
@@ -67,7 +67,7 @@ ggplot2::ggsave("recommend.png", width = 7, height = 7)
 # Future topics
 #####################################################
 
-future_topics <- t |> 
+future_topics <- t |>
   dplyr::select(starts_with("Which future training topics would you like to be provided by the training host"))
 names(future_topics) <- "future_topic"
 future_topics <- future_topics |> dplyr::filter(!is.na(future_topic))
@@ -77,7 +77,7 @@ readr::write_lines(future_topics$future_topic, "future_topics.txt")
 # Other feedback
 #####################################################
 
-comments <- t |> 
+comments <- t |>
   dplyr::select(starts_with("Do you have any additional comments?"))
 names(comments) <- "comment"
 comments <- comments |> dplyr::filter(!is.na(comment))
@@ -92,14 +92,14 @@ t <- read_data()
 # Select confidence questions
 t <- t |> dplyr::select(dplyr::starts_with("I "))
 
-t <- t |> 
+t <- t |>
   dplyr::mutate_all(~ replace(., . == "I can absolutely do this!", 5)) |>
   dplyr::mutate_all(~ replace(., . == "I have good confidence I can do this", 4)) |>
   dplyr::mutate_all(~ replace(., . == "I have some confidence I can do this", 3)) |>
   dplyr::mutate_all(~ replace(., . == "I have low confidence I can do this", 2)) |>
   dplyr::mutate_all(~ replace(., . == "I have no confidence I can do this", 1)) |>
   dplyr::mutate_all(~ replace(., . == "I don't know even what this is about ...?", 0)) |>
-  dplyr::mutate_all(~ replace(., . == "I did not attend that session", NA)) 
+  dplyr::mutate_all(~ replace(., . == "I did not attend that session", NA))
 t$i <- seq(1, nrow(t))
 
 t_tidy <- tidyr::pivot_longer(t, cols = starts_with("I", ignore.case = FALSE))
@@ -113,7 +113,7 @@ n_ratings <- length(t_tidy$answer[!is.na(t_tidy$answer)])
 mean_confidence <- mean(t_tidy$answer[!is.na(t_tidy$answer)])
 
 ggplot2::ggplot(t_tidy, ggplot2::aes(x = answer)) +
-  ggplot2::geom_histogram() + 
+  ggplot2::geom_histogram() +
   ggplot2::labs(
     title = "All confidences",
     caption = paste0(
@@ -126,7 +126,7 @@ ggplot2::ggplot(t_tidy, ggplot2::aes(x = answer)) +
 ggplot2::ggsave(filename = "all_confidences.png", width = 4, height = 2)
 
 ggplot2::ggplot(t_tidy, ggplot2::aes(x = answer)) +
-  ggplot2::geom_histogram() + 
+  ggplot2::geom_histogram() +
   ggplot2::facet_grid(rows = "question", scales = "free_y") +
   ggplot2::theme(
     strip.text.y = ggplot2::element_text(angle = 0),
@@ -141,23 +141,21 @@ ggplot2::ggsave(filename = "confidences_per_question.png", width = 7, height = 7
 average_confidences <- dplyr::group_by(t_tidy, question) |> dplyr::summarise(mean = mean(answer))
 average_confidences$mean <- round(average_confidences$mean, digits = 2)
 
-# Keep these chronologically
-readr::write_csv(average_confidences, file = "average_confidences.csv")
-
 # Sort by value
 average_confidences <- average_confidences |> dplyr::arrange(mean)
 average_confidences$question <- as.factor(average_confidences$question)
 average_confidences$question <- reorder(
-  x = average_confidences$question, 
+  x = average_confidences$question,
   X = order(average_confidences$mean),
   decreasing = TRUE
 )
-  
+readr::write_csv(average_confidences, file = "average_confidences.csv")
+
 
 average_average_confidence <- mean(average_confidences$mean)
 
 ggplot2::ggplot(
-  average_confidences, 
+  average_confidences,
   ggplot2::aes(y = question, x = mean)
 ) +
   ggplot2::geom_bar(stat = "identity") +
@@ -189,4 +187,3 @@ testthat::expect_true(all(t_sessions_taught %in% t_tidy$question))
 confidences_on_taught_sessions <- t_tidy |> dplyr::filter(question %in% t_sessions_taught)
 success_score <- mean(confidences_on_taught_sessions$answer) / 5.0
 readr::write_lines(x = success_score, "success_score.txt")
-
